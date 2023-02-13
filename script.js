a400b7db748f190b84d179349c4d0d1bf7604a31
Divyansh Singh <singh.divyansh1802@gmail.com>
2023-02-13 12:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+const app = document.getElementById("app");
+const swap = document.getElementById("swap");
+
+swap.addEventListener("click", swapTheme);
+
+function swapTheme() {
+  if (app.classList.contains("day")) {
+    app.classList.remove("day");
+    app.classList.add("night");
+    swap.classList.remove("button_day");
+    swap.classList.add("button_night");
+  } else {
+    app.classList.remove("night");
+    app.classList.add("day");
+    swap.classList.remove("button_night");
+    swap.classList.add("button_day");
+  }
+}
